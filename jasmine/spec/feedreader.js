97a313bbb42adccbcf9cfe4d4f559a2d618a1f4e
Tamar Auber <mischegoss@gmail.com>
2018-10-03 10:23:18 -0400

    First Test Suite Done

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,16 +31,30 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('all urls defined and not empty', function() {
+             for(let i = 0; i < allFeeds.length; i++){
+             expect(allFeeds[i].url).toBeDefined();
+             expect(allFeeds[i].url.length).not.toBe(0);
+           }
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+    it('all names defined and not empty', function() {
+        for(let i = 0; i < allFeeds.length; i++){
+        expect(allFeeds[i].name).toBeDefined();
+        expect(allFeeds[i].name.length).not.toBe(0);
+      }
     });
 
+      });
 
     /* TODO: Write a new test suite named "The menu" */
+$(function() {
+  describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -53,7 +67,7 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+  });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
